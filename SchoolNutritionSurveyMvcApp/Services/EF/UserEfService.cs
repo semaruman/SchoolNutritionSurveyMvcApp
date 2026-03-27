342d3277e70df0647c6935cc7592b23c4b5b3984
Semen <semen.ruman@gmail.com>
2026-03-27 05:33:44 +0300

    Добавил реализацию интерфейса IUserService для работы с БД MySql через EF

---
+++ SchoolNutritionSurveyMvcApp/Services/EF/UserEfService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolNutritionSurveyMvcApp.Data;
+using SchoolNutritionSurveyMvcApp.Services.Interfaces;
+
+namespace SchoolNutritionSurveyMvcApp.Services.EF
+{
+    public class UserEfService : IUserService
+    {
+        public void AddToDatabase(UserFormModel user)
+        {
+            using var dbContext = new ApplicationDbContext();
+            dbContext.UserResults.Add(user);
+            dbContext.SaveChanges();
+        }
+
+        public List<UserFormModel> GetUsers()
+        {
+            using var dbContext = new ApplicationDbContext();
+            return dbContext.UserResults.AsNoTracking().ToList();
+        }
+    }
+}
